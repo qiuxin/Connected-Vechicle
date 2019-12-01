3670c760cfc112a9fee844dab81fccb78c9a810a
robertqiu <qiuxincsu@hotmail.com>
2019-12-01 12:06:22 +0800

    Create TestCompileCode_Single_push_logs.sh

---
+++ TestCompileCode_Single_push_logs.sh
@@ -0,0 +1,14 @@
+# Deploying logs to LF Nexus log server ##
+# BUILD_NUMBER and JOB_NAME should be set by Jenkins
+
+NEXUS_URL=https://nexus.akraino.org
+SILO=tencent
+JENKINS_HOSTNAME=54.218.53.101
+JOB_NAME=TestCompileCode
+BUILD_URL="${JENKINS_HOSTNAME}/var/lib/jenkins/jobs/${JOB_NAME}/builds/27"
+NEXUS_PATH="${SILO}/job/${JOB_NAME}/27/"
+/usr/local/python3/bin/lftools deploy logs $NEXUS_URL $NEXUS_PATH $BUILD_URL
+/usr/local/python3/bin/lftools deploy archives -p '**/*.log' $NEXUS_URL $NEXUS_PATH /var/lib/jenkins/jobs/TestCompileCode/builds/27
+
+echo "Logs uploaded to $NEXUS_URL/content/sites/logs/$NEXUS_PATH"
+
